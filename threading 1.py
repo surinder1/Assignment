6f4f473ef700ec4aa00df1e90633c010146c1b00
surinder1 <33355133+surinder1@users.noreply.github.com>
2018-07-01 23:24:53 +0530

    Update threading 1.py

--- threading 1.py
+++ threading 1.py
@@ -1,19 +1,19 @@
 #Q1. Create a threading process such that it sleeps for 5 seconds and then prints out a message.
 
-# method-1
-# import threading
-# from threading import Thread
-# import time 
-# def display():
-	# print("child thread:",threading.current_thread().getName())
-	# bt=time.time()
-	# time.sleep(3)
-	# print("child thread:",threading.current_thread())
-	# et=time.time()
-	# print("total time taken:",et-bt)
+ method-1
+ import threading
+ from threading import Thread
+ import time 
+ def display():
+	 print("child thread:",threading.current_thread().getName())
+	 bt=time.time()
+	 time.sleep(3)
+	 print("child thread:",threading.current_thread())
+	 et=time.time()
+	 print("total time taken:",et-bt)
 
-# t=Thread(target=display)
-# t.start()
+ t=Thread(target=display)
+ t.start()
 
 #Q2. Make a thread that prints numbers from 1-10, waits for 1 sec between
 
